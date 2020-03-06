056a0924b42dbbda43aabd615203f4872ebfd5b2
karsten332 <karsten.sand@haugnett.no>
2020-03-06 10:03:13 +0100

    refactor

--- frontend/components/category/productgridCard.jsx
+++ frontend/components/category/productgridCard.jsx
@@ -5,24 +5,24 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export const ProductgridCard = () => {
 
-
+    // change color of hearthfunction
     const [heartColor, setheartColor] = useState("black");
 
-    function changecolor() {
+    function changeHeartcolor() {
         if (heartColor === "black") {
             setheartColor("red")
         } else {
             setheartColor("black")
         }
     }
-
+    ///// end chage color of hearthfunction
 
     return (
 
         <div className="d-flex">
             <div className="card card-1">
                 <div className="pr-3 row justify-content-end">
-                    <a onClick={(() => changecolor())}><FaHeart color={heartColor} /></a>
+                    <a onClick={(() => changeHeartcolor())}><FaHeart color={heartColor} /></a>
                 </div>
                 <div className="product-pic"> <img className="pic1" src="https://trnbackend.herokuapp.com/files/SPF50+Anthelios.jpg" /> </div>
                 <small className="category"> lepper</small>
