de7d15ed70275b6548789c7e2be107174ddc3b08
LegoLine <line_ronning93@hotmail.com>
2020-03-06 13:29:28 +0100

    Working on layout :rocket:
    Trying to figure out how to resize images using reactstrap/bootstrap. Adding colors to the containers to get a better overview.

--- frontend/pages/produktside.js
+++ frontend/pages/produktside.js
@@ -1,13 +1,36 @@
 import React from "react";
-import Logo from "../components/Logo/Logo";
 import { Button } from "reactstrap";
 
 const Produktside = () => (
   <div>
     <h1>Dette er produktsiden</h1>
-    <Logo />
+    <div className="container-fluid px-1 py-1 mx-auto bg-primary">
+      <div className="row justify-content-center px-3 bg-info">
+        <div className="block text-left bg-success">
+          <div className="container min-vh-10 bg-warning">
+            <img
+              src="https://trnbackend.herokuapp.com/files/SPF50+Anthelios.jpg"
+              alt="Logo"
+              className="img-fluid d-none d-md-block"
+            />
+            <h2>PRODUKTINFORMASJON</h2>
+            <p>
+              Lorizzle ipsizzle hizzle sit amizzle, consectetuer own yo' the
+              bizzle. Phat sapien velizzle, shizzlin dizzle volutpat,
+              suscipizzle stuff, gravida izzle, funky fresh. Pellentesque yo
+              tortor. Sheezy erizzle. Sizzle go to hizzle fizzle mah nizzle
+              break yo neck, yall for sure pizzle. Maurizzle pellentesque nibh
+              izzle turpizzle. Fo shizzle my nizzle fo shizzle my nizzle
+              tortizzle. Sure boofron rhoncizzle sizzle. In hac habitasse we
+              gonna chung dictumst. Fo dapibizzle. Curabitizzle tellus bling
+              bling, pretizzle eu, yo mamma crunk, eleifend vitae, nunc. Fo
+              shizzle my nizzle suscipit. Integer rizzle shiznit sed purizzle.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
     <h3>Produktinformasjon</h3>
-    <Button color="danger">We did it!</Button>
   </div>
 );
 
