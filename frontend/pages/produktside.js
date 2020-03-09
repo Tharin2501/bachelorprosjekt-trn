7bd404af9724b6660a86eb997a1b9f6b5708f9b4
LegoLine <line_ronning93@hotmail.com>
2020-03-09 13:38:50 +0100

    Changes to layout :rocket:
    La bildet og produktnavn i <Row>-element.

--- frontend/pages/produktside.js
+++ frontend/pages/produktside.js
@@ -1,47 +1,37 @@
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, Row, Col } from "reactstrap";
 
 const Produktside = () => (
   <div className="bg-info w-100 h-100 p-3 d-inline-block overflow-auto">
     <h1>Dette er produktsiden</h1>
-    <div className="container text-left bg-warning h-auto w-50">
-      <img
-        className="mh-25 w-25"
-        src="https://trnbackend.herokuapp.com/files/SPF50+Anthelios.jpg"
-      />
-      <div className="bg-success text-right">TEST</div>
-    </div>
+    <Row>
+      <Col xs="6" className="text-left bg-warning h-auto w-50">
+        <img
+          className="mh-25 w-25"
+          src="https://trnbackend.herokuapp.com/files/SPF50+Anthelios.jpg"
+        />
+      </Col>
+      <Col xs="6" className="bg-success text-left overflow-auto w-50">
+        <h2>PRODUKTNAVN</h2>
+        <h3>Pris</h3>
+      </Col>
+    </Row>
   </div>
 );
 
 export default Produktside;
 
 /**
- * 
- * <div className="container-fluid px-3 py-3 mx-auto bg-primary">
-      <div className="row justify-content-center px-3 bg-info">
-        <div className="container min-vh-10 bg-warning img">
-          <img
-            src="https://trnbackend.herokuapp.com/files/SPF50+Anthelios.jpg"
-            alt="Logo"
-          />
-        </div>
-        <div className="container min-vh-90 bg-secondary">
-          <h2>PRODUKTINFORMASJON</h2>
-          <p>
-            Lorizzle ipsizzle hizzle sit amizzle, consectetuer own yo' the
-            bizzle. Phat sapien velizzle, shizzlin dizzle volutpat, suscipizzle
-            stuff, gravida izzle, funky fresh. Pellentesque yo tortor. Sheezy
-            erizzle. Sizzle go to hizzle fizzle mah nizzle break yo neck, yall
-            for sure pizzle. Maurizzle pellentesque nibh izzle turpizzle. Fo
-            shizzle my nizzle fo shizzle my nizzle tortizzle. Sure boofron
-            rhoncizzle sizzle. In hac habitasse we gonna chung dictumst. Fo
-            dapibizzle. Curabitizzle tellus bling bling, pretizzle eu, yo mamma
-            crunk, eleifend vitae, nunc. Fo shizzle my nizzle suscipit. Integer
-            rizzle shiznit sed purizzle.
-          </p>
-        </div>
-      </div>
-    </div>
-    <h3>Produktinformasjon</h3>
+ * <p>
+          Lorizzle ipsizzle hizzle sit amizzle, consectetuer own yo' the bizzle.
+          Phat sapien velizzle, shizzlin dizzle volutpat, suscipizzle stuff,
+          gravida izzle, funky fresh. Pellentesque yo tortor. Sheezy erizzle.
+          Sizzle go to hizzle fizzle mah nizzle break yo neck, yall for sure
+          pizzle. Maurizzle pellentesque nibh izzle turpizzle. Fo shizzle my
+          nizzle fo shizzle my nizzle tortizzle. Sure boofron rhoncizzle sizzle.
+          In hac habitasse we gonna chung dictumst. Fo dapibizzle. Curabitizzle
+          tellus bling bling, pretizzle eu, yo mamma crunk, eleifend vitae,
+          nunc. Fo shizzle my nizzle suscipit. Integer rizzle shiznit sed
+          purizzle.
+        </p>
  */
