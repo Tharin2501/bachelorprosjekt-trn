58b57fb48c8f47963b87dd1b4c1970dde46f45a1
karsten332 <karsten.sand@haugnett.no>
2020-03-13 15:25:21 +0100

    Moved JSX  of productPAge to component

--- frontend/components/productPage/produktside.jsx
+++ frontend/components/productPage/produktside.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Row, Col, Breadcrumb, BreadcrumbItem } from "reactstrap";
-import ProduktTab from "../components/ProduktTab";
+import ProduktTab from "../ProduktTab";
 import {
   FaShoppingCart,
   FaShoppingBasket,
---
+++ frontend/pages/produktside.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Produktside from "../components/productPage/produktside"
+
+const productSide = () => (
+    <div>
+        <h1>Dette er produkt siden</h1>
+        <Produktside></Produktside>
+    </div>
+
+);
+
+export default productSide;
