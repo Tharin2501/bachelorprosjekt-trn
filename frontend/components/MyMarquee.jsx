e5ac940f07694cc7258f367b565768aaee1157ff
Tharin <tharin2501@hotmail.no>
2020-03-17 15:23:03 +0100

    Marquee bugfix done
    Marquee is now responsive and have correct spacing between list items

--- frontend/components/MyMarquee.jsx
+++ frontend/components/MyMarquee.jsx
@@ -3,32 +3,16 @@ import Ticker from 'react-ticker'
 
 const names = ["Norges største parfymeri", "Alltid åpent", "Ingen moms"];
 
-/*
-export const MyMarquee = () => (
-    <div style={{backgroundColor: "lightgrey"}}>
-        <Ticker direction="toRight" mode="chain" speed={10}>
-
-            {(index) => (
-                <div className="row px-4">
-                    {names.map(name =>
-                        <li key={name}> {name} </li>)
-                        .reduce((prev, curr) => [prev, <div key={name} className="px-2"/>, curr])}
-                </div>
-            )}
-        </Ticker>
-    </div>
-);
-*/
-
+// If you don’t pass in an initial value, reduce will assume the first item in your array is your initial value
 export const MyMarquee = () => (
     <div style={{backgroundColor: "lightgrey"}}>
         <Ticker direction="toRight" mode="chain" speed={10}>
 
             {(index) => (
                 <div className="marquee-container">
-                    {names.map(name => <li key={name}> {name} </li>)
-                        .reduce((prev, curr) => [prev, <div key={name} className="px-4"/>, curr])
-                    }
+                    {names.map(name =>
+                        <li key={name}> {name} </li>)
+                        .reduce((accumulator, curr,) => [accumulator, <div key={name} className="px-4"/>, curr], [])}
                 </div>
             )}
         </Ticker>
