ab9b6aa2e33f3f2fe58f3e9857e8474a29225ae3
Tharin <tharin2501@hotmail.no>
2020-03-17 13:58:24 +0100

    Working on Marqueen fix
    Marqueen is responsive for mobile, but needs space between list items

--- frontend/components/MyMarquee.jsx
+++ frontend/components/MyMarquee.jsx
@@ -3,6 +3,7 @@ import Ticker from 'react-ticker'
 
 const names = ["Norges største parfymeri", "Alltid åpent", "Ingen moms"];
 
+/*
 export const MyMarquee = () => (
     <div style={{backgroundColor: "lightgrey"}}>
         <Ticker direction="toRight" mode="chain" speed={10}>
@@ -12,6 +13,22 @@ export const MyMarquee = () => (
                     {names.map(name =>
                         <li key={name}> {name} </li>)
                         .reduce((prev, curr) => [prev, <div key={name} className="px-2"/>, curr])}
+                </div>
+            )}
+        </Ticker>
+    </div>
+);
+*/
+
+export const MyMarquee = () => (
+    <div style={{backgroundColor: "lightgrey"}}>
+        <Ticker direction="toRight" mode="chain" speed={10}>
+
+            {(index) => (
+                <div className="marquee-container">
+                    {names.map(name => <li key={name}> {name} </li>)
+                        .reduce((prev, curr) => [prev, <div key={name} className="px-4"/>, curr])
+                    }
                 </div>
             )}
         </Ticker>
