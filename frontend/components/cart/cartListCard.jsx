c3c8f4120db20c8354ac838f4f14d2aa4ff76780
karsten332 <karsten.sand@haugnett.no>
2020-03-19 15:57:36 +0100

    Fixed favorite list and cart product bug

--- frontend/components/cart/cartListCard.jsx
+++ frontend/components/cart/cartListCard.jsx
@@ -2,6 +2,13 @@ import React, { useState, useEffect, useContext } from "react"
 import { FaHeart, FaMinusCircle, FaPlusCircle } from 'react-icons/fa';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { removeOneProduct } from "../cookieHandler"
+import Link from "next/link";
+
+/*
+    <Link href={{ pathname: "/produktside", query: { id: productcard.id } }}>
+            <a><h5 className="product-name"> {productName}</h5></a>
+          </Link>
+          */
 
 import Cookie from "js-cookie"
 // heavy influence https://bbbootstrap.com/snippets/shopping-cart-checkout-payment-options-86973257
@@ -19,7 +26,11 @@ const CartListCard = (props) => {
 
     const [numberOfProducts, setNumberOfProducts] = useState(productContext.quantity);
 
-
+    /*
+      <Link href={{ pathname: "/produktside", query: { id: productcard.id } }}>
+              <a><h5 className="product-name"> {productName}</h5></a>
+            </Link>
+            */
 
     return (
 
@@ -27,9 +38,9 @@ const CartListCard = (props) => {
         <div className="row d-flex justify-content-center border-top">
             <div className="col-5">
                 <div className="rod d-flex">
-                    <div className="book">  <img src={"https://trnbackend.herokuapp.com" + productContext.image} className="book-img"></img></div>
+                    <div className="book">  <Link href={{ pathname: "/produktside", query: { id: productContext.id } }}><img src={"https://trnbackend.herokuapp.com" + productContext.image} className="book-img"></img></Link></div>
                     <div className="my-auto flex-column d-flex pad-left">
-                        {<h6 className="mob-text">{productContext.name}</h6>}
+                        <Link href={{ pathname: "/produktside", query: { id: productContext.id } }}><a><h6 className="mob-text">{productContext.name}</h6></a></Link>
 
                     </div>
                 </div>
