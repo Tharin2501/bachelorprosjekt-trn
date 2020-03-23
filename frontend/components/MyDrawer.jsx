6943024d98874afc1f27bb89a6a1b1325cb3c52e
Tharin <tharin2501@hotmail.no>
2020-03-23 17:19:39 +0100

    working on refactoring to hooks
    i hate my life

--- frontend/components/MyDrawer.jsx
+++ frontend/components/MyDrawer.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useState} from "react";
 import Drawer from 'react-motion-drawer-improved';
 import {GiHamburgerMenu} from "react-icons/gi";
 import {MySubMenu} from "./layout/layout";
@@ -64,7 +64,7 @@ export default class MyDrawer extends Component {
 
                                     <a onClick={() => this.setState({openLeft: true})}>
                                         <DrawerContent/>
-                                        <DrawerContent2/>
+                                        <DrawerContent3/>
                                     </a>
                                 </li>
                             </ul>
@@ -79,44 +79,22 @@ export default class MyDrawer extends Component {
 }
 
 
-export class DrawerContent extends Component {
-    state = {
-        openLeft: false,
-        drawerStyle: `
-{
-  "background": "#F9F9F9",
-  "boxShadow": "rgba(0, 0, 0, 0.188235) 0px 10px 20px, rgba(0, 0, 0, 0.227451) 0px 6px 6px"
-}`,
-        relativeWidth: false,
-        width: 300,
-        noTouchOpen: false,
-        noTouchClose: false,
+export function DrawerContent(){
+
+    const [openLeft, setOpenLeft] = useState(false);
+    const [width, setWidth] = useState(300);
+    const [noTouchOpen, setNoTouchOpen] = useState(false);
+    const [noTouchClose, setNoTouchClose] = useState(false);
+
+    const drawerProps = {
+        overlayColor: "rgba(255,255,255,0.6)",
     };
 
-
-
-    render() {
-
-
-        const {
-            drawerStyle: stringDrawerStyle,
-            openLeft,
-            noTouchOpen,
-            noTouchClose
-        } = this.state;
-
-        let drawerStyle = {};
-
-        const drawerProps = {
-            overlayColor: "rgba(255,255,255,0.6)", drawerStyle
-        };
-
-        return (
-
+    return(
+        <div>
             <div>
-                <div>
                 <ul>
-                    <li className="nav_submenu-item" onClick={() => this.setState({openLeft: true})}>
+                    <li className="nav_submenu-item" onClick={() => setOpenLeft({openLeft: true})}>
                         <div className="myImg">
                             <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
                         </div>
@@ -125,87 +103,52 @@ export class DrawerContent extends Component {
                 </ul>
 
                 <Drawer {...drawerProps}
-                        width={this.state.width}
+                        width={width}
                         fadeOut
                         open={openLeft}
-                        onChange={open => this.setState({openLeft: open})}
                         noTouchOpen={noTouchOpen}
                         noTouchClose={noTouchClose}>
                     {MyMakeup}
                 </Drawer>
-                </div>
-
-
             </div>
-
-
-        );
-    }
+        </div>
+    );
 }
 
+export function DrawerContent3() {
 
-export class DrawerContent2 extends Component {
-    state = {
-        openLeft: false,
-        drawerStyle: `
-{
-  "background": "#F9F9F9",
-  "boxShadow": "rgba(0, 0, 0, 0.188235) 0px 10px 20px, rgba(0, 0, 0, 0.227451) 0px 6px 6px"
-}`,
-        relativeWidth: false,
-        width: 300,
-        noTouchOpen: false,
-        noTouchClose: false,
+    const [openLeft, setOpenLeft] = useState(false);
+    const [width, setWidth] = useState(300);
+    const [noTouchOpen, setNoTouchOpen] = useState(false);
+    const [noTouchClose, setNoTouchClose] = useState(false);
+
+    const drawerProps = {
+        overlayColor: "rgba(255,255,255,0.6)",
     };
 
-
-
-    render() {
-
-
-        const {
-            drawerStyle: stringDrawerStyle,
-            openLeft,
-            noTouchOpen,
-            noTouchClose
-        } = this.state;
-
-        let drawerStyle = {};
-
-        const drawerProps = {
-            overlayColor: "rgba(255,255,255,0.6)", drawerStyle
-        };
-
-        return (
-
+    return(
+        <div>
             <div>
-                <div>
-                    <ul>
-                        <li className="nav_submenu-item" onClick={() => this.setState({openLeft: true})}>
-                            <div className="myImg">
-                                <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                            </div>
-                            <h1>HEI</h1>
-                        </li>
-                    </ul>
+                <ul>
+                    <li className="nav_submenu-item" onClick={() => setOpenLeft({openLeft: true})}>
+                        <div className="myImg">
+                            <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        </div>
+                        <h1>Hudpleie</h1>
+                    </li>
+                </ul>
 
-                    <Drawer {...drawerProps}
-                            width={this.state.width}
-                            fadeOut
-                            open={openLeft}
-                            onChange={open => this.setState({openLeft: open})}
-                            noTouchOpen={noTouchOpen}
-                            noTouchClose={noTouchClose}>
-                        {MyMakeup2}
-                    </Drawer>
-                </div>
-
-
+                <Drawer {...drawerProps}
+                        width={width}
+                        fadeOut
+                        open={openLeft}
+                        noTouchOpen={noTouchOpen}
+                        noTouchClose={noTouchClose}>
+                    {MyHudpleie}
+                </Drawer>
             </div>
-
-
-        );
-    }
+        </div>
+    );
 }
 
 export const MyMakeup = () => {
@@ -229,7 +172,7 @@ export const MyMakeup = () => {
 }
 
 
-export const MyMakeup2 = () => {
+export const MyHudpleie = () => {
 
     return(
 
