38d5b4043a84b518283114a0f4d02bde075a8d48
karsten332 <karsten.sand@haugnett.no>
2020-03-25 11:22:37 +0100

    Subcatory updated to new subcategoryList system

--- frontend/components/category/subCategoryList.jsx
+++ frontend/components/category/subCategoryList.jsx
@@ -3,61 +3,34 @@ import QuerySubCategory from "../../components/querySubCategory"
 import HorizontalMenuItem from "../../components/category/common/horizontalMenuItem"
 import CATEGORYGETSUBCATEGOREISWITHNAME_QUERY from "../../apollo/queries/Category/CategoryGetSubcategoriesWithName"
 import Link from "next/link";
-const SubCategoryList = (props) => {
-    var ProductsArray = []
+const SubCategoryList = ({ subCategoryList, categoryName }) => {
+    var subcatoryiesArray = []
 
-
-
+    console.log(subCategoryList)
+    console.log(categoryName)
+    //console.log(props.SubCategoryList.sub_categories[0])
 
     return (
-        <QuerySubCategory query={CATEGORYGETSUBCATEGOREISWITHNAME_QUERY} nametoInsert={props.SubCategoryList}>
-            {({ data: { categories } }) => {
-
-                getProductsArray(categories)
-
+        <div className="scrollmenu">
+            <div className="navitem">
+                <Link href={{ pathname: "/category", query: { id: categoryName } }}>
+                    <button type="button" className="btn btn-primary btn-circle btn-sm">Se alt</button>
+                </Link>
+            </div>
+            {subCategoryList.map((category) => {
                 return (
-                    <div className="scrollmenu">
-                        <div className="navitem">
-                            <Link href={{ pathname: "/category", query: { id: props.SubCategoryList } }}>
-                                <button type="button" className="btn btn-primary btn-circle btn-sm">Se alt</button>
-                            </Link>
-                        </div>
-                        {categories[0].sub_categories.map((category) => {
-                            return (
-
-                                <HorizontalMenuItem key={category.id} HorizontalMenuItem={category}></HorizontalMenuItem>
-
-                            )
-                        })}
-                    </div>
+                    <HorizontalMenuItem key={category.id} HorizontalMenuItem={category}></HorizontalMenuItem>
                 )
+            })
+            }
 
-            }}
-        </QuerySubCategory>
+        </div>
+
+
 
     )
-    // all category items
-    function getProductsArray(categories) {
 
-        //console.log(categories[0].sub_categories.length)
-        //console.log(categories[0].sub_categories[1])
-
-        var subcatcount;
-
-        for (subcatcount = 0; subcatcount < categories[0].sub_categories.length; subcatcount++) {
-            ProductsArray = ProductsArray.concat(categories[0].sub_categories[subcatcount].products)
-        }
-        console.log(ProductsArray)
-
-
-    };
 }
 
-
-/*
- for (productCount = 0; productCount < categories[0].sub_categories[subcatcount].length; productCount) {
-            ProductsArray.concat(categories[0].sub_categories[subcatcount].products)
-        }
-        */
 
 export default SubCategoryList
--- frontend/pages/category.js
+++ frontend/pages/category.js
@@ -3,15 +3,25 @@ import { useRouter } from "next/router";
 import SubCategoryList from "../components/category/subCategoryList"
 import ProductsComonent from "../components/category/productsComponent";
 import Highlight3Articles from "../components/category/highlight3Articles"
-
+import CATEGORYGETSUBCATEGOREISWITHNAME_QUERY from "../apollo/queries/Category/CategoryGetSubcategoriesWithName"
+import QuerySubCategory from "../components/querySubCategory";
 //   <Link href={{ pathname: "/produktside", query: { id: productcard.id } }}>
 const Category = () => {
-
+  var ProductsArray = []
   const router = useRouter()
   return (
     <div>
+      <QuerySubCategory query={CATEGORYGETSUBCATEGOREISWITHNAME_QUERY} nametoInsert={router.query.id}>
+        {({ data: { categories } }) => {
+          getProductsArray(categories)
 
-      <SubCategoryList SubCategoryList={router.query.id}></SubCategoryList>
+          return (
+            // first
+            <SubCategoryList subCategoryList={categories[0].sub_categories} categoryName={router.query.id}></SubCategoryList>
+          )
+        }}
+      </QuerySubCategory>
+
 
 
       <Highlight3Articles ></Highlight3Articles>
@@ -19,6 +29,25 @@ const Category = () => {
       <ProductsComonent ProductsComonent={router.query.id}></ProductsComonent>
     </div >
   );
+
+  // all category items
+  function getProductsArray(categories) {
+
+    //console.log(categories[0].sub_categories.length)
+    //console.log(categories[0].sub_categories[1])
+
+    var subcatcount;
+
+    for (subcatcount = 0; subcatcount < categories[0].sub_categories.length; subcatcount++) {
+      ProductsArray = ProductsArray.concat(categories[0].sub_categories[subcatcount].products)
+    }
+    console.log(ProductsArray)
+
+
+  };
+
+
+
 };
 // Alkohol
 export default Category;
