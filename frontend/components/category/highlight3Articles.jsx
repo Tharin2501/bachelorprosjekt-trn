e6e65a9a88c1106c64f304589a0e8ebbc02b5e38
karsten332 <karsten.sand@haugnett.no>
2020-03-27 14:57:45 +0100

    Revert "Test with bigger col on hudpleievelger"
    This reverts commit 0e7d2133b4eefcab667c4befb92540dd0f57aaa4.

--- frontend/components/category/highlight3Articles.jsx
+++ frontend/components/category/highlight3Articles.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Link from "next/link";
-import { Button } from 'reactstrap';
-import { useRouter } from "next/router";
+import {Button} from 'reactstrap';
+import {useRouter} from "next/router";
 
 // https://www.w3schools.com/howto/howto_css_button_on_image.asp USE!
 const Highlight3Articles = (props) => {
@@ -11,8 +11,8 @@ const Highlight3Articles = (props) => {
         <div className="container">
             <div className="row">
 
-                <div className="col-md-8">
-                    <img src="/images/articles/hudpleievelger_test.png" alt="logo" />
+                <div className="col-sm-8">
+                    <img src="/images/articles/hudpleievelger_test.png" alt="logo"/>
 
                     <div className="text-block">
                         <h3>Hudpleievelgeren</h3>
@@ -27,21 +27,21 @@ const Highlight3Articles = (props) => {
 
                 <div className="col-sm-4 pt-3">
                     <div className="article_container">
-                        <img src="/images/articles/folk.jpg" alt="logo" />
+                        <img src="/images/articles/folk.jpg" alt="logo"/>
                         <Link href="#">
                             <button className="btn">les mer</button>
                         </Link>
                     </div>
 
                     <div className="row-sm-4 pt-3 article_container">
-                        <img src="/images/articles/flerefolk.jpg" alt="logo" />
+                        <img src="/images/articles/flerefolk.jpg" alt="logo"/>
                         <Link href="#">
                             <button className="btn">les mer</button>
                         </Link>
                     </div>
                 </div>
             </div>
-            <hr />
+            <hr/>
         </div>
     );
 }
