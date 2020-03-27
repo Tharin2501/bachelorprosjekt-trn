25a02c78f52ac776c592e2d9e7be7d15f847227e
Tharin <tharin2501@hotmail.no>
2020-03-27 11:03:15 +0100

    Added css for article boxes

--- frontend/components/productPage/ArticleBoxes.jsx
+++ frontend/components/productPage/ArticleBoxes.jsx
@@ -6,15 +6,23 @@ export const ArticleBoxes = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-sm-8">
+
+                <div className="col-sm-8 article_container">
                     <img src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <button className="btn">Button</button>
                 </div>
+
                 <div className="col-sm-4">
+                    <div className="article_container">
                     <img src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                    <div className="row-sm-4">
+                        <button className="btn">Button</button>
+                    </div>
+                    <div className="row-sm-4 article_container">
                         <img src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <button className="btn">Button</button>
                     </div>
                 </div>
+
             </div>
             <hr/>
         </div>
