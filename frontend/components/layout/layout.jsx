0a27bde0e1578ca17924d00d9f70a86abbc956e0
karsten332 <karsten.sand@haugnett.no>
2020-03-31 20:43:37 +0200

    Update layout.jsx

--- frontend/components/layout/layout.jsx
+++ frontend/components/layout/layout.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react"
 import Link from "next/link";
 import { FaHeart, FaShoppingCart } from 'react-icons/fa';
+import { FiUser } from "react-icons/fi";
+import { GoSearch } from "react-icons/go";
+import { AiOutlineMail } from "react-icons/ai";
 import {
   Collapse,
   NavbarToggler,
@@ -16,28 +19,22 @@ import {
   InputGroup,
   InputGroupAddon,
   InputGroupText,
-  Input
+  Input,
 } from "reactstrap";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { GoSearch } from "react-icons/go";
 import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu';
 import "rc-menu/assets/index.css"
 import Cookie from "js-cookie";
 import { MyDrawer } from "../MyDrawer";
 
-
 /*
 export const MyHeader = (props) => {
-
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen(prevState => !prevState);
   const [totalprice, setTotalprice] = useState(0);
-
-
   useEffect(() => {
     getCartPrice()
   }, [])
-
   function getCartPrice() {
     if (typeof window !== "undefined") {
       var newtotalprice = 0
@@ -46,16 +43,13 @@ export const MyHeader = (props) => {
         console.log("YAS")
         return;
       }
-
       cart.map((item, i) => {
         newtotalprice += item.quantity * item.price
       })
       //console.log(newtotalprice)
       setTotalprice(newtotalprice)
     }
-
   }
-
   return (
     <div>
       <Navbar color="light" light expand="md">
@@ -110,7 +104,6 @@ export const MyHeader = (props) => {
         </div>
       </Navbar>
       <MySearchbar>
-
       </MySearchbar>
     </div>
   );
@@ -156,6 +149,7 @@ export const MyHeader = () => {
       cart.map((item, i) => {
         newtotalprice += item.quantity * item.price
       })
+      console.log(newtotalprice)
       setTotalprice(newtotalprice)
     }
 
@@ -175,8 +169,8 @@ export const MyHeader = () => {
             <a className="nav-item" href="favorites"><FaHeart color="black" /></a>
           </div>
           <div className="col">
-            profil
-                    </div>
+            <FiUser />
+          </div>
           <div className="col">
             <a className="nav-item" href="shoppingcart"><FaShoppingCart color="black " /></a>
             <p>{totalprice}kr</p>
@@ -189,56 +183,207 @@ export const MyHeader = () => {
     </div>
   );
 };
+/*
+export const MyFooter = () => (
+    <div className="container-fluid">
+        <div className="row py-2">
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">Kontakt oss</a>
+                </Link>
+            </div>
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">Om oss</a>
+                </Link>
+            </div>
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">FAQ</a>
+                </Link>
+            </div>
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">Våre butikker</a>
+                </Link>
+            </div>
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">Karriere og ledige stillinger</a>
+                </Link>
+            </div>
+            <div className="col-sm-2">
+                <Link href="#">
+                    <a className="nav-link">Presse</a>
+                </Link>
+            </div>
+        </div>
+        <div className="row px-1">
+            <div className="col-lg-sm-6">
+                <Link href="#">
+                    <a className="nav-link">
+                        <img src="/images/trn-logos/heinman_footer.png" width="70px" height="70px" alt="logo"/>
+                    </a>
+                </Link>
+            </div>
+            <div className="col-lg-sm-6">
+                <Link href="#">
+                    <a className="nav-link">
+                        <img src="/images/trn-logos/trn_footer.png" width="70px" height="70px" alt="logo"/>
+                    </a>
+                </Link>
+            </div>
+        </div>
+    </div>
+);
+*/
+
+export const NewsLetter = () => {
+
+  const subscribeToNewsletter = () => {
+    // register input from inputfield + check if checkbox is checked. Presist email to db
+    console.log("Subscribe to newsletter here")
+  };
+
+  return (
+    <div className="container-fluid newsletter-style">
+      <div className="row">
+        <div className="col-sm-1">
+        </div>
+        <div className="col-sm py-5">
+          <h3>Hold deg oppdatert</h3>
+          <div className="w-100 py-1" />
+          <p>Vårt nyhetsbrev gir deg inspirasjon, oppdateringer på våre spesialtilbud og kampanjer</p>
+          <div className="w-100 py-1" />
+        </div>
+
+        <div className="col-sm align-self-center">
+          <div>
+            <InputGroup>
+              <InputGroupAddon addonType="prepend">
+                <InputGroupText><AiOutlineMail /></InputGroupText>
+              </InputGroupAddon>
+              <Input placeholder="Din e-post" />
+            </InputGroup>
+            <div className="w-100 py-1" />
+          </div>
+
+
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="myCheckbox" />
+            <label className="form-check-label" htmlFor="myCheckbox">
+              <p style={{ fontWeight: "bold" }}>
+                Ja, jeg har lest og godkjent
+                                <Link href="#">
+                  <a> betingelsene</a>
+                </Link>
+              </p>
+            </label>
+          </div>
+        </div>
+
+        <div className="col-sm pt-1 align-self-center mb-4">
+          <button className="newsletter-btn" onClick={subscribeToNewsletter}>
+            Meld meg på!
+                    </button>
+          <div className="w-100 py-3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export const MyFooter = () => (
-  <div className="container-fluid">
-    <div className="row py-2">
-      <div className="col-sm-2">
-        <Link href="#">
-          <a className="nav-link">Kontakt oss</a>
-        </Link>
+  <div className="container-fluid footer-style pt-2">
+    <div className="py-2">
+      <img src="/images/trn-logos/heinman_footer.png" width="70px" height="70px" alt="logo" />
+    </div>
+    <div className="row py-2 px-2">
+      <div className="col-sm-3">
+        {/* Åpningstider*/}
+        <h3 className="footer-color">Åpningstider</h3>
+        <div className="w-100 py-2" />
+        <p>Duty-Free butikkene er alltid åpne.</p>
+        <Link href="#"><a className="footer-color">Se våre åpningstider her.</a></Link>
+        <div className="w-100 py-4" />
+
+        {/* Telefon*/}
+        <h3 className="footer-color">Telefon</h3>
+        <p className="footer-color">+47 32 23 09 00</p>
+        <div className="w-100 py-2" />
+        <p>
+          Kontakt oss i tidsrommet mandag til fredag 08:00 til 16:00, med unntak av helligdager.
+                </p>
+        <div className="w-100 py-1" />
       </div>
-      <div className="col-sm-2">
+      {/* Snarveier */}
+      <div className="col-sm-3">
+        <h3 className="footer-color">Snarveier</h3>
+        <div className="w-100 py-2" />
         <Link href="#">
-          <a className="nav-link">Om oss</a>
+          <a className="footer-color">Kvote</a>
         </Link>
-      </div>
-      <div className="col-sm-2">
+        <div className="w-100 py-2" />
         <Link href="#">
-          <a className="nav-link">FAQ</a>
+          <a className="footer-color">Produkter</a>
         </Link>
-      </div>
-      <div className="col-sm-2">
+        <div className="w-100 py-2" />
         <Link href="#">
-          <a className="nav-link">Våre butikker</a>
+          <a className="footer-color">Klikk og hent</a>
         </Link>
-      </div>
-      <div className="col-sm-2">
+        <div className="w-100 py-2" />
         <Link href="#">
-          <a className="nav-link">Karriere og ledige stillinger</a>
+          <a className="footer-color">Tilbud</a>
         </Link>
-      </div>
-      <div className="col-sm-2">
+        <div className="w-100 py-2" />
         <Link href="#">
-          <a className="nav-link">Presse</a>
+          <a className="footer-color">Tips og Trender</a>
+        </Link>
+        <div className="w-100 py-2" />
+      </div>
+      {/* Informasjon*/}
+      <div className="col-sm-3">
+        <h3 className="footer-color">Informasjon</h3>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Kundeservice</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Ofte stillte spørsmål</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Reklamasjoner og retur</a>
+        </Link>
+        <div className="w-100 py-2" />
+      </div>
+      {/* Om Oss*/}
+      <div className="col-sm-3">
+        <h3 className="footer-color">Om Oss</h3>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Kontakt oss</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Om oss</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Våre butikker</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Karriere og ledige stillinger</a>
+        </Link>
+        <div className="w-100 py-2" />
+        <Link href="#">
+          <a className="footer-color">Presse</a>
         </Link>
       </div>
     </div>
-    <div className="row px-1">
-      <div className="col-lg-sm-6">
-        <Link href="#">
-          <a className="nav-link">
-            <img src="/images/trn-logos/heinman_footer.png" width="70px" height="70px" alt="logo" />
-          </a>
-        </Link>
-      </div>
-      <div className="col-lg-sm-6">
-        <Link href="#">
-          <a className="nav-link">
-            <img src="/images/trn-logos/trn_footer.png" width="70px" height="70px" alt="logo" />
-          </a>
-        </Link>
-      </div>
-    </div>
+
   </div>
 );
