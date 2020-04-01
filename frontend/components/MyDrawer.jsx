0d9cb901ead73fefe871dc9d30850bcb652a93a5
karsten332 <karsten.sand@haugnett.no>
2020-04-01 15:02:02 +0200

    eh

--- frontend/components/MyDrawer.jsx
+++ frontend/components/MyDrawer.jsx
@@ -1,92 +1,125 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
 import Drawer from 'react-motion-drawer-improved';
-import {GiHamburgerMenu} from "react-icons/gi";
-import {MySubMenu} from "./layout/layout";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Link from "next/link";
 
 export const MyDrawer = () => {
-
     const [openLeft, setOpenLeft] = useState(false);
+
+    const handleOpenLeftChange = () => setOpenLeft(!openLeft)
+
+    return (
+        <div>
+            <div>
+                <ul className="left">
+                    <li style={{ cursor: "pointer", height: "100%" }}>
+                        <a onClick={() => setOpenLeft({ openLeft: true })}>
+                            <GiHamburgerMenu />
+                        </a>
+
+                    </li>
+                </ul>
+            </div>
+            <MainMenu openLeft={openLeft} handleOpenLeftChange={handleOpenLeftChange}></MainMenu>
+        </div>
+    )
+}
+
+export const MainMenu = ({ openLeft }, handleOpenLeftChange) => {
+
+    //const [currentOpenLeft, setCurrentOpenLeft] = useState(false);
+
+
+
+
+    //const setOpenLeft = () => setCurrentOpenLeft(!currentOpenLeft)
+    /*
+        useEffect(() => {
+            setCurrentOpenLeft(openLeft)
+        }, [openLeft])
+    
+    */
+    //handleOpenLeftChange()
     const [width, setWidth] = useState(300);
     const [noTouchOpen, setNoTouchOpen] = useState(false);
     const [noTouchClose, setNoTouchClose] = useState(false);
     const [overLay, setOverlay] = useState("rgba(255,255,255,0.6)");
 
     return (
+        <Drawer overlayColor={overLay}
+            width={width}
+            fadeOut
+            open={openLeft}
+            noTouchOpen={noTouchOpen}
+            noTouchClose={noTouchClose}>
 
-        <div>
-            <div>
-                <ul className="left">
-                    <li style={{cursor: "pointer", height: "100%"}}>
-                        <a onClick={() => setOpenLeft({openLeft: true})}>
-                            <GiHamburgerMenu/>
-                        </a>
+            <div style={{ backgroundColor: `grey`, width: "100%", height: "100%" }}>
+                <ul>
+                    <li className="nav_submenu-item">
+                        <a>Test</a>
+                    </li>
+                    <li className="nav_submenu-item">
+                        <a><MySubMenu></MySubMenu></a>
                     </li>
                 </ul>
             </div>
+        </Drawer>
+    )
 
-            <Drawer overlayColor={overLay}
-                    width={width}
-                    fadeOut
-                    open={openLeft}
-                    noTouchOpen={noTouchOpen}
-                    noTouchClose={noTouchClose}>
 
-                <div style={{backgroundColor: `grey`, width: "100%", height: "100%"}}>
-                    <ul>
-                        <li className="nav_submenu-item">
-                            <a><DrawerContent_Makeup/></a>
-                        </li>
-                        <li className="nav_submenu-item">
-                            <a><DrawerContent_Hudpleie/></a>
-                        </li>
-                    </ul>
-                </div>
-            </Drawer>
-        </div>
-    );
-};
+}
 
-export const DrawerContent_Makeup = () => {
-
+export const MySubMenu = () => {
     const [openLeft, setOpenLeft] = useState(false);
+    var testing
     const [width, setWidth] = useState(300);
     const [noTouchOpen, setNoTouchOpen] = useState(false);
     const [noTouchClose, setNoTouchClose] = useState(false);
     const [overLay, setOverlay] = useState("rgba(255,255,255,0.6)");
+
+
+    const [openLefttest, setOpenLefttest] = useState(false);
+
+    const test = () => {
+        //setOpenLefttest(!openLefttest)
+        console.log(openLefttest)
+
+    }
 
     return (
         <div>
             <ul>
-                <li className="nav_submenu-item" onClick={() => setOpenLeft({openLeft: true})}>
+                <li className="nav_submenu-item" onClick={() => setOpenLeft({ openLeft: true })}>
                     <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo" />
                     </div>
                     <h1>Makeup</h1>
                 </li>
             </ul>
 
+
             <Drawer overlayColor={overLay}
-                    width={width}
-                    fadeOut
-                    open={openLeft}
-                    noTouchOpen={noTouchOpen}
-                    noTouchClose={noTouchClose}>
+                width={width}
+                fadeOut
+                open={openLeft}
+                noTouchOpen={noTouchOpen}
+                noTouchClose={noTouchClose}
+                onChange={test()}
+            >
                 {MyMakeup}
             </Drawer>
         </div>
     );
 }
 
-
 export const MyMakeup = () => {
 
     return (
-        <div style={{backgroundColor: `grey`, width: "100%", height: "100%"}}>
+        <div style={{ backgroundColor: `grey`, width: "100%", height: "100%" }}>
             <ul>
                 <li className="nav_submenu-item">
                     <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo" />
                     </div>
                     <Link href="makeup">
                         <h1 className="nav-link">All Makeup</h1>
@@ -94,7 +127,7 @@ export const MyMakeup = () => {
                 </li>
                 <li className="nav_submenu-item">
                     <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo" />
                     </div>
                     <Link href="#">
                         <h1 className="nav-link">Ansikt</h1>
@@ -102,7 +135,7 @@ export const MyMakeup = () => {
                 </li>
                 <li className="nav_submenu-item">
                     <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
+                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo" />
                     </div>
                     <Link href="#">
                         <h1 className="nav-link">Øyne</h1>
@@ -114,70 +147,3 @@ export const MyMakeup = () => {
 };
 
 
-export function DrawerContent_Hudpleie() {
-
-    const [openLeft, setOpenLeft] = useState(false);
-    const [width, setWidth] = useState(300);
-    const [noTouchOpen, setNoTouchOpen] = useState(false);
-    const [noTouchClose, setNoTouchClose] = useState(false);
-
-    const drawerProps = {
-        overlayColor: "rgba(255,255,255,0.6)",
-    };
-
-    return (
-        <div>
-            <ul>
-                <li className="nav_submenu-item" onClick={() => setOpenLeft({openLeft: true})}>
-                    <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                    </div>
-                    <h1>Hudpleie</h1>
-                </li>
-            </ul>
-
-            <Drawer {...drawerProps}
-                    width={width}
-                    fadeOut
-                    open={openLeft}
-                    noTouchOpen={noTouchOpen}
-                    noTouchClose={noTouchClose}>
-                {MyHudpleie}
-            </Drawer>
-        </div>
-    );
-}
-
-export const MyHudpleie = () => {
-
-    return (
-        <div style={{backgroundColor: `grey`, width: "100%", height: "100%"}}>
-            <ul>
-                <li className="nav_submenu-item">
-                    <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                    </div>
-                    <Link href="hudpleie">
-                        <h1 className="nav-link">All Hudpleie</h1>
-                    </Link>
-                </li>
-                <li className="nav_submenu-item">
-                    <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                    </div>
-                    <Link href="#">
-                        <h1 className="nav-link">Ansikt</h1>
-                    </Link>
-                </li>
-                <li className="nav_submenu-item">
-                    <div className="myImg">
-                        <img className="myImg" src="/images/carousel/placeholder1.jpg" alt="logo"/>
-                    </div>
-                    <Link href="#">
-                        <h1 className="nav-link">Kropp</h1>
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    );
-};
--- frontend/components/layout/layout.jsx
+++ frontend/components/layout/layout.jsx
@@ -27,88 +27,7 @@ import "rc-menu/assets/index.css"
 import Cookie from "js-cookie";
 import { MyDrawer } from "../MyDrawer";
 
-/*
-export const MyHeader = (props) => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const toggle = () => setDropdownOpen(prevState => !prevState);
-  const [totalprice, setTotalprice] = useState(0);
-  useEffect(() => {
-    getCartPrice()
-  }, [])
-  function getCartPrice() {
-    if (typeof window !== "undefined") {
-      var newtotalprice = 0
-      var cart = Cookie.getJSON("cartStorage")
-      if (cart === undefined || cart.length === 0) {
-        console.log("YAS")
-        return;
-      }
-      cart.map((item, i) => {
-        newtotalprice += item.quantity * item.price
-      })
-      //console.log(newtotalprice)
-      setTotalprice(newtotalprice)
-    }
-  }
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <div className="mr-auto">
-          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
-            <DropdownToggle color="transparent">
-              <GiHamburgerMenu />
-            </DropdownToggle>
-            <DropdownMenu modifiers={{
-              setModifiers: {
-                enabled: true,
-                fn: (data) => {
-                  return {
-                    ...data,
-                    styles: {
-                      ...data.styles,
-                      overflow: 'auto',
-                      minHeight: "500px",
-                      minWidth: "500px"
-                    },
-                  };
-                },
-              },
-            }}
-            >
-              <DropdownItem header>Alle produkter</DropdownItem>
-              <Link href="profile"><a title="min profil"><DropdownItem>Dynamisk brukerprofil</DropdownItem></a></Link>
-              <DropdownItem divider />
-              <Link href="lojalitet"><a title="lojalitet"><DropdownItem>Tax Free and Me</DropdownItem></a></Link>
-              <DropdownItem divider />
-              <Link href="parfyme"><a title="parfyme"><DropdownItem>Parfyme</DropdownItem></a></Link>
-              <DropdownItem divider />
-              <Link href="category"><a title="makeup"><DropdownItem>Makeup</DropdownItem></a></Link>
-              <DropdownItem divider />
-              <Link href="hudpleie"><a title="hudpleie"><DropdownItem>Hudpleie</DropdownItem></a></Link>
-              <DropdownItem divider />
-            </DropdownMenu>
-          </Dropdown>
-        </div>
-        <div className="col-4">
-          <a className="nav-item" href="#">Searchbar</a>
-        </div>
-        <div className="mx-auto">
-          <a href="index"><img src="/images/logo.png" width="50px" height="55px" alt="logo" /></a>
-        </div>
-        <div className="col-2">
-          <a className="nav-item" href="favorites"><FaHeart color="black" /></a>
-        </div>
-        <div className="ml-auto">
-          <a className="nav-item" href="shoppingcart"><FaShoppingCart color="black" /></a>
-          <p>{totalprice} kr</p>
-        </div>
-      </Navbar>
-      <MySearchbar>
-      </MySearchbar>
-    </div>
-  );
-};
-*/
+
 const MySearchbar = () => {
   return (
     <div className="row mx-auto w-80 pt-3 pb-3">
@@ -183,60 +102,7 @@ export const MyHeader = () => {
     </div>
   );
 };
-/*
-export const MyFooter = () => (
-    <div className="container-fluid">
-        <div className="row py-2">
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">Kontakt oss</a>
-                </Link>
-            </div>
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">Om oss</a>
-                </Link>
-            </div>
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">FAQ</a>
-                </Link>
-            </div>
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">Våre butikker</a>
-                </Link>
-            </div>
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">Karriere og ledige stillinger</a>
-                </Link>
-            </div>
-            <div className="col-sm-2">
-                <Link href="#">
-                    <a className="nav-link">Presse</a>
-                </Link>
-            </div>
-        </div>
-        <div className="row px-1">
-            <div className="col-lg-sm-6">
-                <Link href="#">
-                    <a className="nav-link">
-                        <img src="/images/trn-logos/heinman_footer.png" width="70px" height="70px" alt="logo"/>
-                    </a>
-                </Link>
-            </div>
-            <div className="col-lg-sm-6">
-                <Link href="#">
-                    <a className="nav-link">
-                        <img src="/images/trn-logos/trn_footer.png" width="70px" height="70px" alt="logo"/>
-                    </a>
-                </Link>
-            </div>
-        </div>
-    </div>
-);
-*/
+
 
 export const NewsLetter = () => {
 
