1841c90aa8d86a6c9b6f8ac916d19deb32a14729
Tharin <tharin2501@hotmail.no>
2020-04-20 10:58:37 +0200

    created responsive text box model

--- frontend/pages/artikler.js
+++ frontend/pages/artikler.js
@@ -3,8 +3,18 @@ import React from "react"
 const Artikler = () => {
 
     return(
-        <h1> test123</h1>
+        <div className="justify-content-center text-center">
+            <h1 className="pb-4"> Hent tittel fra strapi</h1>
+            <div className="square pb-5">
+                <div className="content">
+                    <p>This paragraph shoulb be fetched dynamically by strapi.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                    </p>
+                </div>
+            </div>
+        </div>
     )
-}
+};
 
 export default Artikler;
