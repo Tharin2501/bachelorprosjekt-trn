53814fe0fd7f9f85aec5ada08ac821ce90cd939d
Tharin <tharin2501@hotmail.no>
2020-04-20 16:05:44 +0200

    Done with main layout and box model for social media box

--- frontend/pages/artikler.js
+++ frontend/pages/artikler.js
@@ -2,11 +2,31 @@ import React from "react"
 
 const Artikler = () => {
 
-    return(
+    return (
         <div className="justify-content-center text-center">
             <h1 className="pb-4"> Hent tittel fra strapi</h1>
-            <div className="square pb-5">
-                <div className="content">
+
+            <div className="square">
+                <p>This paragraph shoulb be fetched dynamically by strapi.
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                </p>
+            </div>
+
+            {/* fetch image from strapi */}
+            <img src="/images/articles/folk.jpg" alt="logo"/>
+            {ArticleContent()}
+            {SocialMediaContactBox()}
+
+        </div>
+    )
+};
+
+const SocialMediaContactBox = () => {
+    return (
+        <div className="square">
+            <div className="content">
+                <div className="article-main2">
                     <p>This paragraph shoulb be fetched dynamically by strapi.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
@@ -16,5 +36,41 @@ const Artikler = () => {
         </div>
     )
 };
+
+// Main content for article (white box)
+const ArticleContent = () => {
+    return (
+        <div className="square">
+            <div className="content">
+                {/* fetch article content from strapi */}
+                <div className="article-main">
+                    <div>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type
+                            It has survived not only five centuries, but also the leap into electronic
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of
+                        </p>
+                        <h2>Undertittel 1</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and scrambled it to make a type
+                            It has survived not only five centuries, but also the leap into electronic
+                            remaining essentially unchanged. It was popularised in the 1960s with the release of
+                        </p>
+                    </div>
+                    <img src="/images/articles/flerefolk.jpg" alt="logo"/>
+                    <h2>Undertittel 2</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and scrambled it to make a type
+                        It has survived not only five centuries, but also the leap into electronic
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Artikler;
