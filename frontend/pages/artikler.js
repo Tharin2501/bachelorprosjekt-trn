799ada7c6837f9320e3dfed8c448e7548f90599b
Tharin <tharin2501@hotmail.no>
2020-04-21 15:30:40 +0200

    Added product cards component

--- frontend/pages/artikler.js
+++ frontend/pages/artikler.js
@@ -1,13 +1,16 @@
 import React from "react"
+import {AiOutlineMail} from "react-icons/ai"
+import Link from "next/link";
 
+// NOTE: Ikke bruk noe annet enn <p> tag i de responsive divene
 const Artikler = () => {
 
     return (
-        <div className="justify-content-center text-center">
+        <div className="justify-content-center text-center"> {/* Fjern text-center kanskje??*/}
             <h1 className="pb-4"> Hent tittel fra strapi</h1>
 
             <div className="square">
-                <p>This paragraph shoulb be fetched dynamically by strapi.
+                <p>This paragraph should be fetched dynamically by strapi.
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                 </p>
@@ -17,21 +20,28 @@ const Artikler = () => {
             <img src="/images/articles/folk.jpg" alt="logo"/>
             {ArticleContent()}
             {SocialMediaContactBox()}
-
         </div>
     )
 };
 
+// Social media contact content (Pink box)
 const SocialMediaContactBox = () => {
     return (
         <div className="square">
             <div className="content">
-                <div className="article-main2">
+                <div className="socialMediaContact-container">
+                    <p>Undertittel 1 er kult men den går ikke ut</p>
                     <p>This paragraph shoulb be fetched dynamically by strapi.
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                     </p>
+                    {/* fetch image from strapi */}
+                    <img src="/images/articles/folk.jpg" alt="logo" width={350}/>
                 </div>
+            </div>
+            <div className="text-left mt-3">
+                <p><AiOutlineMail style={{margin: 15}}/> Skrevet av author fra strapi </p>
+                <hr/>
             </div>
         </div>
     )
@@ -43,7 +53,7 @@ const ArticleContent = () => {
         <div className="square">
             <div className="content">
                 {/* fetch article content from strapi */}
-                <div className="article-main">
+                <div className="article-main-container pt-5">
                     <div>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
@@ -51,7 +61,7 @@ const ArticleContent = () => {
                             It has survived not only five centuries, but also the leap into electronic
                             remaining essentially unchanged. It was popularised in the 1960s with the release of
                         </p>
-                        <h2>Undertittel 1</h2>
+                        <h3>Undertittel 1</h3>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                             when an unknown printer took a galley of type and scrambled it to make a type
@@ -67,10 +77,41 @@ const ArticleContent = () => {
                         It has survived not only five centuries, but also the leap into electronic
                         remaining essentially unchanged. It was popularised in the 1960s with the release of
                     </p>
+                    {ArticleProductCard()}
                 </div>
             </div>
         </div>
     )
 }
+
+export const ArticleProductCard = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card">
+                        <img className="card-img-top" src="/images/articles/flerefolk.jpg" alt="Card image cap"></img>
+                        <div className="card-body">
+                            <h5 className="card-title">Produkt tittel</h5>
+                            <p className="card-text">Her kommer produktbeskrivelsen</p>
+                            <a href="#" className="btn btn-primary">Kjøp</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card">
+                        <img className="card-img-top" src="/images/articles/flerefolk.jpg" alt="Card image cap"></img>
+                        <div className="card-body">
+                            <h5 className="card-title">Produkt tittel</h5>
+                            <p className="card-text">Her kommer produktbeskrivelsen</p>
+                            <a href="#" className="btn btn-primary">Kjøp</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
 
 export default Artikler;
