c1624f82be22827864eafab5d7217e5967a05fd0
karsten332 <karsten.sand@haugnett.no>
2020-04-24 14:19:16 +0200

    added quiz layout

--- frontend/components/skincareRoutinePicker/quiz/quizController.jsx
+++ frontend/components/skincareRoutinePicker/quiz/quizController.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import QuizQuestion from "./quizQuestion"
+import { Button } from 'reactstrap';
 const QuizController = () => {
-    <head>
 
-    </head>
-
+    // varibal for om svaret er valgt 
+    // så en func for legge det til listen
     // gå til produt side i stedenfor se omtale eller vis antall  <SkincareCaruselCardCheckboxList></SkincareCaruselCardCheckboxList>
     return (
-        <div >
-            <p> Testcontroller</p>
+        <div align="center" >
             <QuizQuestion></QuizQuestion>
+            <Button > Neste</Button>
         </div>
     );
 };
--- frontend/components/skincareRoutinePicker/quiz/quizQuestion.jsx
+++ frontend/components/skincareRoutinePicker/quiz/quizQuestion.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import { Container, Row, Col, Button } from 'reactstrap';
 const QuizQuestion = () => {
     <head>
 
@@ -6,9 +7,14 @@ const QuizQuestion = () => {
 
     // gå til produt side i stedenfor se omtale eller vis antall  <SkincareCaruselCardCheckboxList></SkincareCaruselCardCheckboxList>
     return (
-        <div >
-            <p>Faen</p>
-        </div>
+        <Container className="quizQuestionContainer">
+            <h2 align="center"> Hva heter du ?</h2>
+            <Button color="secondary" size="lg" block>Lars</Button>
+            <Button color="secondary" size="lg" block>Karsten</Button>
+            <Button color="secondary" size="lg" block>Bernie</Button>
+            <Button color="secondary" size="lg" block>Vil ikke oppggi</Button>
+
+        </Container>
     );
 };
 
