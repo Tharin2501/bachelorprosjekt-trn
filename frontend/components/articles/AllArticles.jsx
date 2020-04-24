a7f4d05b02f9be6c8fc1d6f6ce225259523c2ab4
Tharin <tharin2501@hotmail.no>
2020-04-24 16:43:09 +0200

    Added correct attribute for card image

--- frontend/components/articles/AllArticles.jsx
+++ frontend/components/articles/AllArticles.jsx
@@ -11,7 +11,7 @@ const AllArticles = () => {
                 <span className="horizontal-line2">Våre Artikler</span>
             </h3>
             <div className="container-fluid">
-                <div className="row justify-content-center ">
+                <div className="row justify-content-center">
                     <Query query={GETCATEGORIES_QUERY}>
                         {({data: {categories}}) => {
                             console.log(categories)
@@ -19,8 +19,19 @@ const AllArticles = () => {
                                 categories.map((category) => {
                                     return (
                                         <div key={category.id} className="col-lg-sm-6 px-5">
-                                            <div className="card " style={{width: "18rem"}}>
-                                                <div className="card-body ">
+                                            <div className="card text-center" style={{width: "18rem"}}>
+                                                {/* IMAGE*/}
+                                                <div className="card-img-top ">
+                                                    <Link href={{
+                                                        pathname: "/category",
+                                                        query: {id: category.StrapiName}
+                                                    }}>
+                                                        <a className="nav-link"><img
+                                                            src={"https://trnbackend.herokuapp.com" + category.image.url}
+                                                            alt="logo"/></a>
+                                                    </Link>
+                                                </div>
+                                                <div className="card-body">
                                                     {/* TITLE*/}
                                                     <h5 className="card-title">
                                                         <Link href={{
@@ -30,16 +41,6 @@ const AllArticles = () => {
                                                             <a className=".frontpageIconsText">{category.name}</a>
                                                         </Link>
                                                     </h5>
-                                                    <div className="card-text">
-                                                        <Link href={{
-                                                            pathname: "/category",
-                                                            query: {id: category.StrapiName}
-                                                        }}>
-                                                            <a className="nav-link imageSize"><img
-                                                                src={"https://trnbackend.herokuapp.com" + category.image.url}
-                                                                alt="logo"/></a>
-                                                        </Link>
-                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
