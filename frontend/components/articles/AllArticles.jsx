929f04800fa56f6a73f70abc5bdf68b64c8cd509
Tharin <tharin2501@hotmail.no>
2020-04-26 20:51:50 +0200

    Done with new improved layout for all articles

--- frontend/components/articles/AllArticles.jsx
+++ frontend/components/articles/AllArticles.jsx
@@ -33,20 +33,20 @@ const AllArticles = () => {
                                                 </div>
                                                 <div className="card-body">
                                                     <div className="allArticle-main-container">
-                                                    {/* TITLE*/}
-                                                    <h5 className="card-title">
-                                                        <Link href={{
-                                                            pathname: "/category",
-                                                            query: {id: category.StrapiName}
-                                                        }}>
-                                                            <a className=".frontpageIconsText">{category.name}</a>
-                                                        </Link>
-                                                    </h5>
-                                                    {/* INTRODUCTION*/}
-                                                    <p className="card-text">
-                                                    This is the introduction text.
-                                                        Vin til and bør være både frisk og smakskraftig.
-                                                    </p>
+                                                        {/* TITLE*/}
+                                                        <h5 className="card-title">
+                                                            <Link href={{
+                                                                pathname: "/category",
+                                                                query: {id: category.StrapiName}
+                                                            }}>
+                                                                <a className=".frontpageIconsText">{category.name}</a>
+                                                            </Link>
+                                                        </h5>
+                                                        {/* INTRODUCTION*/}
+                                                        <p className="card-text">
+                                                            This is the introduction text.
+                                                            Vin til and bør være både frisk og smakskraftig.
+                                                        </p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -62,4 +62,51 @@ const AllArticles = () => {
     )
 };
 
-export default AllArticles;
+
+// https://www.louistiti.fr/tutoriel-html5-css3-carte-article-ui/33
+const AllArticles2 = () => {
+
+    return (
+        <div>
+            <h3 className="horizontal-line1 pt-4">
+                <span className="horizontal-line2">Våre Artikler</span>
+            </h3>
+            <div className="container-fluid">
+                <div className="row justify-content-center">
+                    <div className="col-12">
+                        <div className="card">
+
+                            <div className="mycard-header">
+                                <img src="/images/articles/folk.jpg" alt="logo"/>
+
+                            </div>
+
+                            <div className="card-body">
+
+                                <div className="card-body-header">
+                                    <h1>Alt du trenger å vite om parfyme</h1>
+                                </div>
+
+
+                                <p className="card-body-description pt-3">
+                                    Parfyme er en blanding av eteriske oljer og aromatiske dufter.
+                                    Det er et kunstverk vi bruker for å uttrykke hvem vi er,
+                                    for å forføre og føle oss attraktive.
+                                    Parfyme spiller en større rolle inn i dagene våre enn vi tror og det kan kobles til
+                                    både minner og følelser.
+                                </p>
+
+                                <div className="pt-5"><h6 style={{color: "#757B82"}}>Skjønnhet og velvære</h6></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+
+
+export default AllArticles2;
--- frontend/pages/artikler.js
+++ frontend/pages/artikler.js
@@ -1,12 +1,12 @@
 import React from "react"
 import Articles from "../components/articles/articles";
-import AllArticles from "../components/articles/AllArticles";
+import AllArticles2 from "../components/articles/AllArticles";
 
 // Side for alle Artikler
 const Artikler = () => {
 
     return (
-        <AllArticles/>
+        <AllArticles2/>
     )
 };
 
