36fdff1c0be65414616c94309bff59701034d9e1
karsten332 <karsten.sand@haugnett.no>
2020-04-29 15:40:12 +0200

    Moved quiz to hudpleievelger page

--- frontend/pages/hudpleievelger.js
+++ frontend/pages/hudpleievelger.js
@@ -1,49 +1,14 @@
 import React from 'react';
-import SkincareCarusel from "../components/skincareRoutinePicker/skincareCarusel.jsx"
-import { Button } from "reactstrap";
-import SkincareDesktopController from "../components/skincareRoutinePicker/skincareDesktopController"
-import SkincareDesktopCard from "../components/skincareRoutinePicker/skincareDesktopCard.jsx"
-const isServer = () => typeof window === `undefined`;
+import QuizController from "../components/skincareRoutinePicker/quiz/quizController"
 
-const oleHenProduct = {
-    stepText: "Steg 1:Rens",
-    imageUrl: "/files/1279254-trnLargeFormat.jpg",
-    productName: "Ole Henriksen green tea Toner",
-    productPrice: "999 kr",
-    productDescription: "Using bootstrap's jumbotron with the particle.js background to make the jumbotron semi-transparent."
-}
-const Hudpleievelger = () => {
+const Hudpleievelger = () => (
 
-    if (!isServer()) {
-        if (window.innerWidth > 700) {
-            return (
-                <SkincareDesktopCard product={oleHenProduct} ></SkincareDesktopCard>
-            )
-        } else {
-            return (
-
-                < SkincareCarusel  >
-
-                </SkincareCarusel >
-
-
-
-            )
-        }
-    }
-
-
-    if (isServer()) {
-        return (
-            null
-        )
-    }
+    <QuizController></QuizController>
 
 
 
 
-
-};
+);
 
 
 
