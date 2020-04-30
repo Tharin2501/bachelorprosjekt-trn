a6f6077071537a103c4f3e379fcd2ff3ff4698a0
karsten332 <karsten.sand@haugnett.no>
2020-04-30 03:07:51 +0200

    Update skincareCaruselController.jsx

--- frontend/components/skincareRoutinePicker/skincareCaruselController.jsx
+++ frontend/components/skincareRoutinePicker/skincareCaruselController.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import SkincareMobileController from "./skincareMobileController.jsx";
-import SkincareDesktopController from "./skincareDesktopController.jsx";
+import SkincareMobileController from "./skincareMobileController";
+import SkincareDesktopController from "./skincareDesktopController";
 
 const isServer = () => typeof window === `undefined`;
 
