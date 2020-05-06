100d6fde79d8b7dff3367e2d13049078a8b3e285
LegoLine <line_ronning93@hotmail.com>
2020-05-06 11:17:47 +0200

    small fixes

--- frontend/apollo/queries/brand/brand.js
+++ frontend/apollo/queries/brand/brand.js
@@ -1,6 +1,7 @@
 import gql from "graphql-tag";
 
 //TODO: Fikse så man henter inn id dynamisk når man trykker på et merke
+//GETPRODUCTSBRAND
 
 const BRAND_QUERY = gql`
   query brands($id: ID!) {
@@ -12,30 +13,14 @@ const BRAND_QUERY = gql`
         url
       }
       products {
-        brand {
-          id
-          name
-          description
-          logo {
-            url
-          }
+        id
+        name
+        description
+        logo {
+          url
         }
       }
     }
   }
 `;
 export default BRAND_QUERY;
-
-/**
- * 
-query brands($id: ID!) {
-    brand(id: $id) {
-      id
-      name
-      description
-      logo {
-        url
-      }
-    }
-  }
- */
