b2da76e45cbe985eee9267bd5bc413f62005a85e
karsten332 <karsten.sand@haugnett.no>
2020-05-12 23:03:49 +0200

    Fixed html lang error and myCarousel css added again.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -10,43 +10,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "react-multi-carousel/lib/styles.css";
 
 
-
 const App = ({ Component, pageProps, apollo }) => {
     const [price, setTotalPrice] = useState(0);
     const ChangeTotalPrice = (newValue) => setTotalPrice(newValue);
     return (
 
         <ApolloProvider client={apollo}>
+
+
+            <Head>
+                {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Staatliches"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
+                />
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
+                <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
+                <script src="https://kit.fontawesome.com/b0dd2d05dd.js" cross0rigin="anonymous"></script>
+            </Head>
+
             <html lang="nb">
-
-                <Head>
-                    {/* <meta name="viewport" content="initial-scale=1.0, width=device-width" /> */}
-                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <link
-                        rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Staatliches"
-                    />
-                    <link
-                        rel="stylesheet"
-                        href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css"
-                    />
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js" />
-                    <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js" />
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js" />
-                    <script src="https://kit.fontawesome.com/b0dd2d05dd.js" cross0rigin="anonymous"></script>
-                </Head>
-
-
                 <body>
+
                     <CartTotalPriceContext.Provider value={{ price, ChangeTotalPrice }}>
 
                         <MyHeader />
                         <Component {...pageProps} />
                         <MyFooter />
+
                     </CartTotalPriceContext.Provider>
 
                 </body>
-            </html>
+            </html >
         </ApolloProvider>
 
 
