2631862a5044dc0cc963e6633dd5f1325c8fe541
karsten332 <karsten.sand@haugnett.no>
2020-05-12 14:58:44 +0200

    removed import statements

--- frontend/pages/merkeside.js
+++ frontend/pages/merkeside.js
@@ -1,21 +1,13 @@
 import React from "react";
 import BrandPage from "../components/brandPage/brandPage";
-import BrandPageOverView from "../components/brandPage/brandPageOverView";
-import SpecificBrandPage from "../components/brandPage/specificBrandPage";
 
 const merkeSide = (props) => {
   return (
     <div>
-      <BrandPageOverView />
+
       <BrandPage />
     </div>
   );
 };
 
 export default merkeSide;
-
-/**
- *
- *
-      <SpecificBrandPage />
- */
--- frontend/pages/merkesidedetalj.js
+++ frontend/pages/merkesidedetalj.js
@@ -1,13 +1,9 @@
 import React from "react";
-import BrandPage from "../components/brandPage/brandPage";
-import BrandPageOverView from "../components/brandPage/brandPageOverView";
 import SpecificBrandPage from "../components/brandPage/specificBrandPage";
 import { useRouter } from "next/router";
 import Query from "../components/query";
 import BRAND_QUERY from "../apollo/queries/brand/brand";
-import QuerySubCategory from "../components/querySubCategory";
-import CATEGORYGETSUBCATEGOREISWITHNAME_QUERY from "../apollo/queries/Category/CategoryGetSubcategoriesWithName";
-//TODO: Fikse horizontal scroll + laste inn produkter fra spesifikt merke
+
 
 const merkesidedetalj = () => {
   const router = useRouter();
