30d30e3c6cda95a75a4736f10ad9fce6b80c30f4
karsten332 <karsten.sand@haugnett.no>
2020-05-13 09:42:13 +0200

    changed color to white

---
+++ frontend/pages/klikkOgHent.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { ClickandCollectBanner } from "../components/Frontpage"
+
+const KlikkOgHent = () => (
+
+return (
+    <h1>klikk Og Hent</h1>
+    <ClickandCollectBanner> </ClickandCollectBanner>
+)
+);
+
+export default klikkOgHent;
