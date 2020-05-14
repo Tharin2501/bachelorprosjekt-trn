b98565def40444d7f57e1e16aeb06f4733048042
karsten332 <karsten.sand@haugnett.no>
2020-05-14 13:48:50 +0200

    Revert "Update hamburgermenuList.jsx"
    This reverts commit 29ef992114571a44af6d19177b96e100849e7016.

--- frontend/components/hamburgermenu/hamburgermenuList.jsx
+++ frontend/components/hamburgermenu/hamburgermenuList.jsx
@@ -99,7 +99,7 @@ const HamburgermenuList = forwardRef((props, ref) => {
                     <div tabindex="0" onClick={() => { handlearticlesClick() }} onKeyDown={() => { checkIfSpaceOrEnterPressed(event) ? handlearticlesClick() : null }}>
 
                         <div className="myImg">
-                            <img className="myImg" src={articlesSVG} alt="" />
+                            <img className="myImg" src={require("../../public/images/artikler2.0.svg").default} alt="" />
                         </div>
 
                         <p className="nav-link">{"Artikler"}</p>
