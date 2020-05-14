2048df44364f57d1b3717d1c696522b5c60c8386
karsten332 <karsten.sand@haugnett.no>
2020-05-14 13:48:46 +0200

    Revert "used svgs loader"
    This reverts commit a211506214d878a70fb9ac524053ca0d97df9c7a.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,7 @@
-
-module.exports = {
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            issuer: {
-                test: /\.(js|ts)x?$/,
-            },
-            use: ['@svgr/webpack'],
-        });
-
-        return config;
-    },
-};
+const withImages = require('next-images')
+module.exports = withImages({
+    esModule: true,
+    webpack(config, options) {
+        return config
+    }
+})
