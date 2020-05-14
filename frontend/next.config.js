cf03148382b91e9e8abc2c99974abffae425e662
karsten332 <karsten.sand@haugnett.no>
2020-05-14 13:49:00 +0200

    Revert "changed nextjs configfile"
    This reverts commit ee97ba49cfa4b71a62bda17bf28e5ba790601a53.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,9 @@
-const fileloader = require('file-loader');
+
 module.exports = {
     webpack(config) {
         config.module.rules.push({
             test: /\.(png|jpe?g|gif|svg)$/i,
-            loader: 'fileloader',
+            loader: 'file-loader',
             options: {
                 outputPath: 'images',
             },
