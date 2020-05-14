d5606db6042468a1a53d4866342f80e03263e948
karsten332 <karsten.sand@haugnett.no>
2020-05-14 13:48:40 +0200

    Revert "convert to es6"
    This reverts commit c966ec9ea51a19662b7a7a98ff0f328463a605ed.

--- frontend/components/hamburgermenu/hamburgermenuList.jsx
+++ frontend/components/hamburgermenu/hamburgermenuList.jsx
@@ -5,7 +5,7 @@ import Query from "../../components/query"
 import HamburgermenuItem from "../../components/hamburgermenu/hamburgermenuItem.jsx"
 import { useRouter } from 'next/router'
 import { checkIfSpaceOrEnterPressed } from "../utils/accessibilityUtil"
-import ArticlesSVG from "../../public/images/artikler2.0.svg";
+import ArticlesSVG from "/images/artikler2.0.svg";
 const HamburgermenuList = forwardRef((props, ref) => {
 
 
